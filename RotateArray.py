6624529645725041d5a8d25caf4e80dcb2be8b1f
abhinavathankar <abhinavathankar@gmail.com>
2020-05-27 22:57:21 +0530

    New File

---
+++ RotateArray.py
@@ -0,0 +1,27 @@
+"""
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Follow up:
+
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+"""
+def rotate_array(nums,k):
+    nums[:]=nums[len(nums)-k:]+nums[0:len(nums)-k]
+
+    return nums
+
+nums=[22,33,44,55,66]
+k=3
+nums=rotate_array(nums,k)
+print(nums)
